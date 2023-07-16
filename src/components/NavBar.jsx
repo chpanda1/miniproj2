f4f5964d2d8f036488da33f48480c9f0bba99e1e
Jasper Navales <jasper.jvdcr@gmail.com>
2023-07-16 14:19:29 +0800

    Created form in contact us, changed color of navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Button, Box, AppBar, Toolbar, IconButton, Typography, } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
+import '../JEM2.png'
+import Navbar from './NavBar.css'
 
 import Styled from 'styled-components';
 
@@ -9,17 +11,18 @@ import { Link } from 'react-router-dom';
 
 function NavBar() {
 
+
     const StyledLink = Styled(Link)`
     color: white; 
     position: relative; 
-    &:hover{color: yellow}; 
+    &:hover{color: #2F3130}; 
     text-decoration: none;
     margin: 3px;`
 
   return (
 
     <Box sx={{ flexGrow: 1 }} component="nav">
-        <AppBar position="static">
+        <AppBar position="fixed" sx={{ backgroundColor: '#426F86'}}>
             <Toolbar>
                 <IconButton
                 size="large"
@@ -28,7 +31,7 @@ function NavBar() {
                 aria-label="menu"
                 sx={{ mr: 2 }}
                 >
-                <MenuIcon />
+                <img src="../JEM2.png" alt="" className='logoSize'/>
                 </IconButton>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     <StyledLink to="/">Home</StyledLink> 
